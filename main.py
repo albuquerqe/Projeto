e9e919fc5899a7bb2d0536a861d18f20a9463d89
Luiz H Albuquerque <134562026+albuquerqe@users.noreply.github.com>
2023-06-27 21:56:39 -0300

    adicionei
    aqui adicionei o display

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ largura = 800
 altura = 500
 tamanho = (largura, altura)
 tela = pygame.display.set_mode(tamanho)
-
+pygame.display.set_caption("SPACE MARKER DA ANA e Luiz")
+gameDisplay = pygame.display.set_mode(tamanho)
+meteoroSound = pygame.mixer.Sound("Space_Machine.mp3")
+clock = pygame.time.Clock()
