d26d9767e65b5607f0882439440660869472fe64
Ana Flavia <132021408+FlorencioMl@users.noreply.github.com>
2023-06-27 22:32:34 -0300

    adc
    adc linha try:

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ pygame.display.set_caption("SPACE MARKER DA ANA e Luiz")
 gameDisplay = pygame.display.set_mode(tamanho)
 meteoroSound = pygame.mixer.Sound("Space_Machine.mp3")
 clock = pygame.time.Clock()
+
+try: 
+    pygame.mixer.music.load("Space_Machine.mp3")
+    pygame.mixer.music.play(-1)
+except pygame.error:
+    print("Erro no audio")
+
+icone_path = "space.png"
+icone = pygame.image.load(icone_path)
+pygame.display.set_icon(icone)  
+
